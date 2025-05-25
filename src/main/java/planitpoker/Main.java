b95d7c0d5a9e2013de44e8ec20fa7c7136a81f55
JimmyChin946 <Judes946@gmail.com>
2025-05-25 13:05:40 -0700

    main

--- src/main/java/planitpoker/Main.java
+++ src/main/java/planitpoker/Main.java
@@ -6,53 +6,43 @@ import javax.swing.*;
  * Main class that creates the JFrame
  * This initially kicks the user to a fresh login page
  *
- * @author Jude Shin 
+ * @author
  */
 public class Main extends JFrame {
-	public Main(int launchNumber) {
-		switch (launchNumber) {
-			case 1:
-				// ----- Login -----
-				LoginController loginController = new LoginController(this);
-				LoginPanel loginPanel = new LoginPanel(loginController);
-				add(loginPanel);
-				break;
+	public Main() {
+		// ----- Login -----
+		LoginController loginController = new LoginController(this);
+		LoginPanel loginPanel = new LoginPanel(loginController);
+		add(loginPanel);
 
-			case 2:
-				// ----- CreateRoom -----
-				CreateRoomController createRoomController = new CreateRoomController(this);
-				CreateRoomPanel createRoomPanel = new CreateRoomPanel(createRoomController);
-				add(createRoomPanel);
-				break;
+		// // ----- CreateRoom -----
+		// CreateRoomController createRoomController = new CreateRoomController(this);
+		// CreateRoomPanel createRoomPanel = new CreateRoomPanel(createRoomController);
+		// add(createRoomPanel);
 
-			case 3:
-				// ----- Export -----
-				ExportController exportController = new ExportController(this);
-				ExportPanel exportPanel = new ExportPanel(exportController);
-				add(exportPanel);
-				break;
-		}
+		// // ----- Export -----
+		// ExportController exportController = new ExportController(this);
+		// ExportPanel exportPanel = new ExportPanel(exportController);
+		// add(exportPanel);
 	}
 
 	public static void main(String[] args) {
-		// in prod, you would only have one main panel
-		// this is just for ease of demonstration 
-		Main loginTestMain = new Main(1);
-		loginTestMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		loginTestMain.setSize(400, 400);
-		loginTestMain.setLocationRelativeTo(null);
-		loginTestMain.setVisible(true);
+		Main login = new Main();
+		login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		login.setSize(400, 400);
+		login.setLocationRelativeTo(null);
+		login.setVisible(true);
 
-		Main createRoomTestMain = new Main(2);
-		createRoomTestMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		createRoomTestMain.setSize(400, 400);
-		createRoomTestMain.setLocationRelativeTo(null);
-		createRoomTestMain.setVisible(true);
+		// Main createRoomTestMain = new Main(2);
+		// createRoomTestMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// createRoomTestMain.setSize(400, 400);
+		// createRoomTestMain.setLocationRelativeTo(null);
+		// createRoomTestMain.setVisible(true);
 
-		Main exportTestMain = new Main(3);
-		exportTestMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		exportTestMain.setSize(400, 400);
-		exportTestMain.setLocationRelativeTo(null);
-		exportTestMain.setVisible(true);
+		// Main exportTestMain = new Main(3);
+		// exportTestMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// exportTestMain.setSize(400, 400);
+		// exportTestMain.setLocationRelativeTo(null);
+		// exportTestMain.setVisible(true);
 	}
 }
