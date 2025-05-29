e246fc57dd133e2534ef6473562e8c2f9fe9bd64
JimmyChin946 <Judes946@gmail.com>
2025-05-28 22:04:15 -0700

    priority is now in the publishItem

--- src/main/java/planitpoker/mqtt/PublishItem.java
+++ src/main/java/planitpoker/mqtt/PublishItem.java
@@ -4,6 +4,7 @@ package planitpoker.mqtt;
  * A datastructure to hold a "message"
  * Holds the name of the topic
  * and the content of the message
+ * and the priority of it (QOS)
  *
  * @author Jude Shin
  *
@@ -11,12 +12,15 @@ package planitpoker.mqtt;
 public class PublishItem {
 	private final String subTopic;
 	private final byte[] message; 
+	private final int qos;
 
-	public PublishItem(String subTopic, byte[] message) {
+	public PublishItem(String subTopic, byte[] message, int qos) {
 		this.subTopic = subTopic; 
 		this.message = message;	
+		this.qos = qos;
 	}
 
 	public String getSubTopic() { return subTopic; }
 	public byte[] getMessage() { return message; }
+	public int getQos() { return qos; }
 }
