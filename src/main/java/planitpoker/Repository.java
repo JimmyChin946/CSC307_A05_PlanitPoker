0a4a2039ed9103ae713f705e29df69a683e07325
JimmyChin946 <Judes946@gmail.com>
2025-05-28 22:44:52 -0700

    host and client types added to repository

--- src/main/java/planitpoker/Repository.java
+++ src/main/java/planitpoker/Repository.java
@@ -12,10 +12,11 @@ import planitpoker.mqtt.PublishItem;
  * @author Jude Shin 
  */
 public class Repository extends PropertyChangeSupport{
-	// var def
 	private static Repository instance;
-	private User currentUser = null; 
-	// private Room currentRoom = null; // TODO : put the singleton in this singleton?
+	private User currentUser; // could be final?
+	// private Room currentRoom = null; // TODO : put this in the singleton? 
+	public enum Type { HOST, CLIENT }
+	private Type type; // could be final?
 	private final String[] votingMethodNames = {"Sequential", "Fibonacci"};;
 	private final Double[][] votingMethodNumbers = {{0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0}, {0.0, 0.5, 1.0, 2.0, 3.0, 5.0, 8.0}};;
 	private Queue<PublishItem> publishQueue;
@@ -37,6 +38,8 @@ public class Repository extends PropertyChangeSupport{
 	public void setCurrentUser (User user) { currentUser = user; }
 	// public Room getCurrentRoom() { return currentRoom; }
 	// public void setCurrentRoom (Room room) { currentRoom = room; }
+	public Type getType() { return type; }
+	public void setType(Type type) { this.type = type; }
 
 	public String[] getVotingMethodNames() { return votingMethodNames; }
 	public Double[][] getVotingMethodNumbers() { return votingMethodNumbers; }
