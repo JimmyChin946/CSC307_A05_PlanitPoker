dd1b31f35a2b5cb7be845b4b56e7a590ca503096
Nathan Lackie <nlackie@calpoly.edu>
2025-05-31 19:24:25 -0700

    Setup results panel

--- src/main/java/planitpoker/voting/VotingPanel.java
+++ src/main/java/planitpoker/voting/VotingPanel.java
@@ -1,9 +1,12 @@
 package planitpoker.voting;
 
 import java.awt.*;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.*;
+import planitpoker.Repository;
 
 /**
  * Where people will vote
@@ -11,18 +14,39 @@ import javax.swing.*;
  *
  * @author Nathan Lackie
  */
-public class VotingPanel extends JPanel {
-	CardsPanel cards;
+public class VotingPanel extends JPanel implements PropertyChangeListener {
+	VotingController votingController;
 
 	public VotingPanel(VotingController votingController) {
 		super();
 
+		this.votingController = votingController;
+
 		setLayout(new GridLayout(1, 2));
 
-		cards = new CardsPanel(votingController, new ArrayList<>(List.of(1, 2, 3, 4)));
-		add(cards);
+		Repository.getInstance().addPropertyChangeListener("votingStarted", this);
+
+		drawUI();
+	}
+
+	private void drawUI() {
+		removeAll();
+
+		if (Repository.getInstance().getVotingStarted()) {
+			add(new CardsPanel(votingController, new ArrayList<>(List.of(1, 2, 3, 4))));
+		} else {
+			add(new ResultsPanel());
+		}
 
 		VotingControlsPanel votingControls = new VotingControlsPanel(votingController);
 		add(votingControls);
+
+		revalidate();
+		repaint();
+	}
+
+	@Override
+	public void propertyChange(PropertyChangeEvent propertyChangeEvent) {
+		drawUI();
 	}
 }
