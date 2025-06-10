67879029747e60a813d38af2d9966453ef776f60
JimmyChin946 <Judes946@gmail.com>
2025-06-10 16:03:41 -0700

    fomatting time remaining string

--- src/main/java/planitpoker/T7StoriesPanel.java
+++ src/main/java/planitpoker/T7StoriesPanel.java
@@ -147,12 +147,21 @@ public class T7StoriesPanel extends JPanel implements PropertyChangeListener {
 		}
 
 		JLabel estimationLabel = new JLabel("" + story.getEstimation(), SwingConstants.CENTER);
-		JLabel timeLabel = new JLabel("" + story.getTime().getSeconds(), SwingConstants.CENTER);
+		JLabel timeLabel = new JLabel(formatSeconds(story.getTime().getSeconds()), SwingConstants.CENTER);
 
 		panel.add(estimationLabel);
 		panel.add(timeLabel);
 
 		return panel;
+	}
+
+
+	private String formatSeconds(long totalSeconds) {
+		long hours = totalSeconds / 3600;
+		long minutes = (totalSeconds % 3600) / 60;
+		long seconds = totalSeconds % 60;
+
+		return String.format("%02d:%02d:%02d", hours, minutes, seconds);
 	}
 
 
